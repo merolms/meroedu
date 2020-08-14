57e562e635570046ae06cf71102f6f82c2e229cd
Dinesh Katwal <medineshkatwal@gmail.com>
2020-08-14 14:48:56 +0530

    Added TestGetAll for course_usecase_test.go

--- app/course/usecase/couse_usecase_test.go
+++ app/course/usecase/couse_usecase_test.go
@@ -1,22 +1,56 @@
 package usecase_test
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"testing"
+	"time"
 
+	ucase "github.com/meroedu/course-api/app/course/usecase"
+	"github.com/meroedu/course-api/app/domain"
 	"github.com/meroedu/course-api/app/domain/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestGetAll(t *testing.T) {
 	mockCourseRepo := new(mocks.CourseRepository)
-	mockCourses := []domain.Course{
+	// mockUserRepo := new(mocks.UserRepository)
+	// mockLessonRepo := new(mocks.LessonRepository)
+	// mockAttachmentRepo:=new(mocks.AttachmentRepository)
+	// mockCategoryRepo:=new(mocks.CategoryRepository)
+	mockListCourse := []domain.Course{
 		domain.Course{
 			ID: 1, Title: "title-1",
 			Author: domain.User{ID: 1}, UpdatedAt: time.Now(), CreatedAt: time.Now(),
 		},
 	}
-	t.Run("success", func(t *testing.T){
-		mockCourseRepo.On("GetAll", mock.Anything, mock.AnythingOfType("string")
-		)	
+	t.Run("success", func(t *testing.T) {
+		mockCourseRepo.On("GetAll", mock.Anything, mock.AnythingOfType("int"),
+			mock.AnythingOfType("int")).Return(mockListCourse, nil).Once()
+
+		start := int(0)
+		limit := int(1)
+		u := ucase.NewCourseUseCase(mockCourseRepo, time.Second*2)
+		list, err := u.GetAll(context.TODO(), start, limit)
+		assert.NoError(t, err)
+		assert.Len(t, list, len(mockListCourse))
+		mockCourseRepo.AssertExpectations(t)
+
+	})
+	t.Run("error-failed", func(t *testing.T) {
+		mockCourseRepo.On("GetAll", mock.Anything, mock.AnythingOfType("int"),
+			mock.AnythingOfType("int")).Return(nil, errors.New("Unexpexted Error")).Once()
+
+		u := ucase.NewCourseUseCase(mockCourseRepo, time.Second*2)
+		start := int(0)
+		limit := int(1)
+		list, err := u.GetAll(context.TODO(), start, limit)
+
+		assert.Error(t, err)
+		assert.Len(t, list, 0)
+		mockCourseRepo.AssertExpectations(t)
 	})
 }
 func TestGetByID(t *testing.T) {
