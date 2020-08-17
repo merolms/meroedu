bc5bde6062f5cd72077974923291f67f283967ae
Dinesh Katwal <medineshkatwal@gmail.com>
2020-08-18 03:31:38 +0545

    Added Tag mysql crud and also added unit test

--- app/domain/tag.go
+++ app/domain/tag.go
@@ -13,7 +13,19 @@ type Tag struct {
 	CreatedAt time.Time `json:"-,omitempty"`
 }
 
-// TagRepository represent the Tag's repository contract
-type TagRepository interface {
+// TagUseCase represent the Tag's repository contract
+type TagUseCase interface {
+	GetAll(ctx context.Context, start int, limit int) ([]Tag, error)
 	GetByID(ctx context.Context, id int64) (Tag, error)
+	UpdateTag(ctx context.Context, Tag *Tag, id int64) error
+	CreateTag(ctx context.Context, Tag *Tag) error
+}
+
+// TagRepository represent the Tag's repository
+type TagRepository interface {
+	GetAll(ctx context.Context, start int, limit int) ([]Tag, error)
+	GetByID(ctx context.Context, id int64) (Tag, error)
+	UpdateTag(ctx context.Context, Tag *Tag) error
+	CreateTag(ctx context.Context, Tag *Tag) error
+	DeleteTag(ctx context.Context, id int64) error
 }
