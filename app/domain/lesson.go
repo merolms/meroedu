c5bd3b69452d16e7d2f0f537f6cfbef8a8b2271e
Dinesh Katwal <medineshkatwal@gmail.com>
2020-08-18 04:08:50 +0545

    Create simple crud for lesson and content

--- app/domain/lesson.go
+++ app/domain/lesson.go
@@ -9,7 +9,7 @@ import (
 type Lesson struct {
 	ID          int64     `json:"id"`
 	CourseID    int64     `json:"course_id,omitempty"`
-	Title       int64     `json:"title,omitempty" validate:"required"`
+	Title       string    `json:"title,omitempty" validate:"required"`
 	Description string    `json:"description,omitempty"`
 	Tags        []Tag     `json:"tags,omitempty"`
 	Contents    []Content `json:"contents,omitempty"`
@@ -17,7 +17,19 @@ type Lesson struct {
 	CreatedAt   time.Time `json:"created_at,omitempty"`
 }
 
-// LessonRepository represent the Lesson's repository contract
-type LessonRepository interface {
+// LessonUseCase represent the Lesson's repository contract
+type LessonUseCase interface {
+	GetAll(ctx context.Context, start int, limit int) ([]Lesson, error)
 	GetByID(ctx context.Context, id int64) (Lesson, error)
+	UpdateLesson(ctx context.Context, Lesson *Lesson, id int64) error
+	CreateLesson(ctx context.Context, Lesson *Lesson) error
+}
+
+// LessonRepository represent the Lesson's repository
+type LessonRepository interface {
+	GetAll(ctx context.Context, start int, limit int) ([]Lesson, error)
+	GetByID(ctx context.Context, id int64) (Lesson, error)
+	UpdateLesson(ctx context.Context, Lesson *Lesson) error
+	CreateLesson(ctx context.Context, Lesson *Lesson) error
+	DeleteLesson(ctx context.Context, id int64) error
 }
---
+++ app/lesson/repository/mysql/mysql_lesson.go
@@ -0,0 +1,151 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/meroedu/meroedu/app/domain"
+	"github.com/sirupsen/logrus"
+)
+
+type mysqlRepository struct {
+	Conn *sql.DB
+}
+
+// InitMysqlRepository will create an object that represent the lesson's Repository interface
+func InitMysqlRepository(db *sql.DB) domain.LessonRepository {
+	return &mysqlRepository{
+		Conn: db,
+	}
+}
+func (m *mysqlRepository) fetch(ctx context.Context, query string, args ...interface{}) (result []domain.Lesson, err error) {
+	rows, err := m.Conn.QueryContext(ctx, query, args...)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	defer func() {
+		errRow := rows.Close()
+		if errRow != nil {
+			logrus.Error(errRow)
+		}
+	}()
+
+	result = make([]domain.Lesson, 0)
+	for rows.Next() {
+		t := domain.Lesson{}
+		err = rows.Scan(
+			&t.ID,
+			&t.Title,
+			&t.UpdatedAt,
+			&t.CreatedAt,
+		)
+
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+		result = append(result, t)
+	}
+
+	return result, nil
+}
+
+func (m *mysqlRepository) GetAll(ctx context.Context, start int, limit int) (res []domain.Lesson, err error) {
+	fmt.Println("Called GetAll Lessons")
+	query := `SELECT id,name, updated_at, created_at FROM lessons ORDER BY created_at DESC LIMIT ?,?`
+
+	res, err = m.fetch(ctx, query, start, limit)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+func (m *mysqlRepository) GetByID(ctx context.Context, id int64) (res domain.Lesson, err error) {
+	query := `SELECT id,name,updated_at,created_at FROM lessons WHERE ID = ?`
+
+	list, err := m.fetch(ctx, query, id)
+	if err != nil {
+		return domain.Lesson{}, err
+	}
+
+	if len(list) > 0 {
+		res = list[0]
+	} else {
+		return res, domain.ErrNotFound
+	}
+
+	return
+}
+
+func (m *mysqlRepository) CreateLesson(ctx context.Context, a *domain.Lesson) (err error) {
+	query := `INSERT  lessons SET title=?, updated_at=? , created_at=?`
+	stmt, err := m.Conn.PrepareContext(ctx, query)
+	if err != nil {
+		logrus.Error("Error while preparing statement ", err)
+		return
+	}
+	res, err := stmt.ExecContext(ctx, a.Title, a.UpdatedAt, a.CreatedAt)
+	if err != nil {
+		logrus.Error("Error while executing statement ", err)
+		return
+	}
+	lastID, err := res.LastInsertId()
+	if err != nil {
+		logrus.Error("Got Error from LastInsertId method: ", err)
+		return
+	}
+	a.ID = lastID
+	return
+}
+
+func (m *mysqlRepository) DeleteLesson(ctx context.Context, id int64) (err error) {
+	query := "DELETE FROM lessons WHERE id = ?"
+
+	stmt, err := m.Conn.PrepareContext(ctx, query)
+	if err != nil {
+		return
+	}
+
+	res, err := stmt.ExecContext(ctx, id)
+	if err != nil {
+		return
+	}
+
+	rowsAfected, err := res.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if rowsAfected != 1 {
+		err = fmt.Errorf("Weird  Behavior. Total Affected: %d", rowsAfected)
+		return
+	}
+
+	return
+}
+func (m *mysqlRepository) UpdateLesson(ctx context.Context, ar *domain.Lesson) (err error) {
+	query := `UPDATE lessons set title=?, updated_at=? WHERE ID = ?`
+
+	stmt, err := m.Conn.PrepareContext(ctx, query)
+	if err != nil {
+		return
+	}
+
+	res, err := stmt.ExecContext(ctx, ar.Title, ar.UpdatedAt, ar.ID)
+	if err != nil {
+		return
+	}
+	affect, err := res.RowsAffected()
+	if err != nil {
+		return
+	}
+	if affect != 1 {
+		err = fmt.Errorf("Weird  Behavior. Total Affected: %d", affect)
+		return
+	}
+
+	return
+}
