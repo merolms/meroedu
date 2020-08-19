3c85ac5d2ebc03e83d2be2aa79f3f0bc0f3f98dd
Dinesh Katwal <medineshkatwal@gmail.com>
2020-08-19 10:04:13 +0545

    Added lesson usecase and it's test cases

--- app/lesson/usecase/lesson_usecase.go
+++ app/lesson/usecase/lesson_usecase.go
@@ -1,2 +1,100 @@
 package usecase
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/meroedu/meroedu/app/domain"
+)
+
+// LessonUseCase ...
+type LessonUseCase struct {
+	lessonRepo     domain.LessonRepository
+	contextTimeOut time.Duration
+}
+
+// NewLessonUseCase will creae new an
+func NewLessonUseCase(c domain.LessonRepository, timeout time.Duration) domain.LessonUseCase {
+	return &LessonUseCase{
+		lessonRepo:     c,
+		contextTimeOut: timeout,
+	}
+}
+
+// GetAll ...
+func (usecase *LessonUseCase) GetAll(c context.Context, start int, limit int) (res []domain.Lesson, err error) {
+	ctx, cancel := context.WithTimeout(c, usecase.contextTimeOut)
+	defer cancel()
+
+	res, err = usecase.lessonRepo.GetAll(ctx, start, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// GetByID ...
+func (usecase *LessonUseCase) GetByID(c context.Context, id int64) (res domain.Lesson, err error) {
+	ctx, cancel := context.WithTimeout(c, usecase.contextTimeOut)
+	defer cancel()
+
+	res, err = usecase.lessonRepo.GetByID(ctx, id)
+	if err != nil {
+		return domain.Lesson{}, err
+	}
+
+	return res, nil
+}
+
+// // GetByTitle ...
+// func (usecase *LessonUseCase) GetByTitle(c context.Context, title string) (res domain.Lesson, err error) {
+// 	ctx, cancel := context.WithTimeout(c, usecase.contextTimeOut)
+// 	defer cancel()
+// 	res, err = usecase.lessonRepo.GetByTitle(ctx, title)
+// 	if err != nil {
+// 		return domain.Lesson{}, err
+// 	}
+// 	return res, nil
+// }
+
+// CreateLesson ..
+func (usecase *LessonUseCase) CreateLesson(c context.Context, lesson *domain.Lesson) (err error) {
+	ctx, cancel := context.WithTimeout(c, usecase.contextTimeOut)
+	defer cancel()
+	// existingLesson, err := usecase.GetByTitle(ctx, lesson.Title)
+	// fmt.Println(existingLesson)
+	// fmt.Println(domain.Lesson{})
+	// if existingLesson != (domain.Lesson{}) {
+	// 	return domain.ErrConflict
+	// }
+	lesson.UpdatedAt = time.Now()
+	lesson.CreatedAt = time.Now()
+	err = usecase.lessonRepo.CreateLesson(ctx, lesson)
+	if err != nil {
+		return
+	}
+	return
+
+}
+
+// UpdateLesson ..
+func (usecase *LessonUseCase) UpdateLesson(c context.Context, lesson *domain.Lesson, id int64) (err error) {
+	ctx, cancel := context.WithTimeout(c, usecase.contextTimeOut)
+	defer cancel()
+	existingLesson, err := usecase.GetByID(ctx, id)
+	fmt.Println(existingLesson)
+	fmt.Println(domain.Lesson{})
+	// if existingLesson != (domain.Lesson{}) {
+	// 	return domain.ErrConflict
+	// }
+	lesson.ID = id
+	lesson.UpdatedAt = time.Now()
+	err = usecase.lessonRepo.UpdateLesson(ctx, lesson)
+	if err != nil {
+		return
+	}
+	return
+
+}
--- app/lesson/usecase/lesson_usecase_test.go
+++ app/lesson/usecase/lesson_usecase_test.go
@@ -1,1 +1,169 @@
-package usecase
+package usecase_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/meroedu/meroedu/app/domain"
+	"github.com/meroedu/meroedu/app/domain/mocks"
+	ucase "github.com/meroedu/meroedu/app/lesson/usecase"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetAll(t *testing.T) {
+	mockLessonRepo := new(mocks.LessonRepository)
+	mockListLesson := []domain.Lesson{
+		domain.Lesson{
+			ID: 1, Title: "title-1",
+		},
+	}
+	t.Run("success", func(t *testing.T) {
+		mockLessonRepo.On("GetAll", mock.Anything, mock.AnythingOfType("int"),
+			mock.AnythingOfType("int")).Return(mockListLesson, nil).Once()
+
+		start := int(0)
+		limit := int(1)
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+		list, err := u.GetAll(context.TODO(), start, limit)
+		assert.NoError(t, err)
+		assert.Len(t, list, len(mockListLesson))
+		mockLessonRepo.AssertExpectations(t)
+
+	})
+	t.Run("error-failed", func(t *testing.T) {
+		mockLessonRepo.On("GetAll", mock.Anything, mock.AnythingOfType("int"),
+			mock.AnythingOfType("int")).Return(nil, errors.New("Unexpexted Error")).Once()
+
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+		start := int(0)
+		limit := int(1)
+		list, err := u.GetAll(context.TODO(), start, limit)
+
+		assert.Error(t, err)
+		assert.Len(t, list, 0)
+		mockLessonRepo.AssertExpectations(t)
+	})
+}
+func TestGetByID(t *testing.T) {
+	mockLessonRepo := new(mocks.LessonRepository)
+	mockLesson := domain.Lesson{
+		Title: "title-1",
+	}
+	t.Run("success", func(t *testing.T) {
+		mockLessonRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(mockLesson, nil).Once()
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		a, err := u.GetByID(context.TODO(), mockLesson.ID)
+
+		assert.NoError(t, err)
+		assert.NotNil(t, a)
+
+		mockLessonRepo.AssertExpectations(t)
+	})
+	t.Run("error-failed", func(t *testing.T) {
+		mockLessonRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(domain.Lesson{}, errors.New("Unexpected")).Once()
+
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		a, err := u.GetByID(context.TODO(), mockLesson.ID)
+
+		assert.Error(t, err)
+		assert.Equal(t, domain.Lesson{}, a)
+
+		mockLessonRepo.AssertExpectations(t)
+	})
+}
+
+// func TestGetByTitle(t *testing.T) {
+// 	mockLessonRepo := new(mocks.LessonRepository)
+// 	mockLesson := domain.Lesson{
+// 		Name: "title-1",
+// 	}
+// 	t.Run("success", func(t *testing.T) {
+// 		mockLessonRepo.On("GetByTitle", mock.Anything, mock.AnythingOfType("string")).Return(mockLesson, nil).Once()
+// 		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+// 		a, err := u.GetByTitle(context.TODO(), mockLesson.Title)
+
+// 		assert.NoError(t, err)
+// 		assert.NotNil(t, a)
+
+// 		mockLessonRepo.AssertExpectations(t)
+// 	})
+// 	t.Run("error-failed", func(t *testing.T) {
+// 		mockLessonRepo.On("GetByTitle", mock.Anything, mock.AnythingOfType("string")).Return(domain.Lesson{}, errors.New("Unexpected")).Once()
+
+// 		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+// 		a, err := u.GetByTitle(context.TODO(), "random")
+
+// 		assert.Error(t, err)
+// 		assert.Equal(t, domain.Lesson{}, a)
+
+// 		mockLessonRepo.AssertExpectations(t)
+// 	})
+// }
+func TestCreateLesson(t *testing.T) {
+	mockLessonRepo := new(mocks.LessonRepository)
+	mockLesson := domain.Lesson{
+		Title: "Hello",
+	}
+
+	t.Run("success", func(t *testing.T) {
+		tempmockLesson := mockLesson
+		tempmockLesson.ID = 0
+		// mockLessonRepo.On("GetByTitle", mock.Anything, mock.AnythingOfType("string")).Return(domain.Lesson{}, domain.ErrNotFound).Once()
+		mockLessonRepo.On("CreateLesson", mock.Anything, mock.AnythingOfType("*domain.Lesson")).Return(nil).Once()
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		err := u.CreateLesson(context.TODO(), &tempmockLesson)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockLesson.Title, tempmockLesson.Title)
+		mockLessonRepo.AssertExpectations(t)
+	})
+	t.Run("existing-title", func(t *testing.T) {
+		// mockLessonRepo.On("GetByTitle", mock.Anything, mock.AnythingOfType("string")).Return(existingCourse, nil).Once()
+		mockLessonRepo.On("CreateLesson", mock.Anything, mock.AnythingOfType("*domain.Lesson")).Return(nil).Once()
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		err := u.CreateLesson(context.TODO(), &mockLesson)
+
+		assert.NoError(t, err)
+		mockLessonRepo.AssertExpectations(t)
+	})
+}
+func TestUpdateLesson(t *testing.T) {
+	mockLessonRepo := new(mocks.LessonRepository)
+	mockLesson := domain.Lesson{
+		ID:    1,
+		Title: "Hello",
+	}
+
+	t.Run("success", func(t *testing.T) {
+		tempmockLesson := mockLesson
+		mockLessonRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(domain.Lesson{}, domain.ErrNotFound).Once()
+		mockLessonRepo.On("UpdateLesson", mock.Anything, mock.AnythingOfType("*domain.Lesson")).Return(nil).Once()
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		err := u.UpdateLesson(context.TODO(), &tempmockLesson, tempmockLesson.ID)
+
+		assert.NoError(t, err)
+		assert.Equal(t, mockLesson.ID, tempmockLesson.ID)
+		mockLessonRepo.AssertExpectations(t)
+	})
+	t.Run("existing-title", func(t *testing.T) {
+		existingCourse := mockLesson
+		mockLessonRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(existingCourse, nil).Once()
+		mockLessonRepo.On("UpdateLesson", mock.Anything, mock.AnythingOfType("*domain.Lesson")).Return(nil).Once()
+		u := ucase.NewLessonUseCase(mockLessonRepo, time.Second*2)
+
+		err := u.UpdateLesson(context.TODO(), &mockLesson, existingCourse.ID)
+
+		assert.NoError(t, err)
+		mockLessonRepo.AssertExpectations(t)
+	})
+}
