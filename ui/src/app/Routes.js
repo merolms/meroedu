f29753c6854c7eecdc47ba0b4b97592aab01815d
Dinesh Katwal <medineshkatwal@gmail.com>
2020-08-23 05:15:12 +0545

    Created course grid component

--- ui/src/app/Routes.js
+++ ui/src/app/Routes.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 import React from "react";
-import TestContainer from "../containers/TestContainer";
+import TestContainer from "./containers/TestContainer";
+import CourseContainer from "./courses/containers/CourseContainer";
 
 const Routes = () => {
     return (
         <Switch>
             <Route exact path="/" render={() => <div>Welcome</div>}/>
             <Route exact path="/test" component={TestContainer}/>
+            <Route exact path="/courses" component={CourseContainer}/>
         </Switch>
     )
 }
---
+++ ui/src/app/courses/containers/CourseContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from "react-redux";
+// import { asyncSetTestValue, setTestValue } from "../../redux/actions/testActions";
+import CourseGridItem from '../components/CourseGridItem';
+import { setTestValue, asyncSetTestValue } from '../../../redux/actions/testActions';
+import {Grid, GridRow} from 'semantic-ui-react';
+class CourseContainer extends React.Component {
+    render() {
+        let rows=[]
+        for (let i = 0; i <20; i++) {
+            rows.push(<Grid.Column key={i} style={{margin: '0px', marginBottom: '25px'}}> <CourseGridItem {...this.props} /></Grid.Column>)
+        }
+        return (
+                <Grid stackable>
+                    <GridRow columns={1} only="mobile">{rows}</GridRow>
+                    <GridRow columns={2} only="tablet">{rows}</GridRow>
+                    <GridRow columns={4} only="computer">{rows}</GridRow>
+                </Grid>
+                
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        test: state.test
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        setValue: (value) => {
+            dispatch(setTestValue(value))
+        },
+        asyncSetValue: (value) => {
+            dispatch(asyncSetTestValue(value));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CourseContainer);
